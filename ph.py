576e5ca3bc09fe030d5b71c55352ce6d96aff91b
command <rekreb81@gmail.com>
2017-01-22 14:49:05 +0200

    Update ph.py
    update <3

--- ph.py
+++ ph.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
-# ccc GNU GPLv2 ccc
-
-import subprocess,os,re,time
+import os
+import re
+import subprocess
+import threading
+import time
 
 def hint(appname):
 	try:
@@ -11,6 +12,7 @@ def hint(appname):
 		appname = appname[-1]
 	except:
 		appname = appname.strip()
+	#print (appname)
 	uyari = "\"UYARI !\""
 	mesaj = "\"Yüksek CPU kullanımı tespit edildi \n%s \"" %(appname)
 	komut = "notify-send "+uyari+" "+mesaj+" -t 6000 -i hint" 
@@ -22,25 +24,25 @@ def check_prc():
 	bash_out = subprocess.check_output("ps aux",stderr=subprocess.STDOUT,shell=True)
 	satirlar = bash_out.splitlines()
 	for i in satirlar:
-		i = re.sub('([\s]+)', ' ', i)
+		i = re.sub('([\s]+)', ' ', str(i))
 		dizi.append(i)
 	dizi.pop(0) # ilk satırı sil
 	for i in dizi:
 		i = i.split(" ")
 		cpu_usage,name = float(i[2]),str(i[10])
 		if cpu_usage > HIGH:
-			hint(name) 
+			findapp = threading.Thread(target=hint(name))
+			findapp.start()
+			findapp.join()
 
 def main():
 	while 1:
 		check_prc()
-		time.sleep(10)
+		time.sleep(5)
 	return 0
 
 HIGH = 90
-#tüm tarayıcıların ve javanın .....
-EXCEPTS = ["firefox","chrome","chromium","java"]
+EXCEPTS = ["firefox","chrome","chromium","java","inox"]
 
 if __name__ == '__main__':
 	main()
-	
