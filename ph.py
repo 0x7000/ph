9005d982dc710387d003b6ecafc461f11b3d884a
command <rekreb81@gmail.com>
2019-04-27 16:26:22 +0300

    fix fix fix

--- ph.py
+++ ph.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+'''
+GPLv2 lisansıyle dağıtılır
+'''
 
 import os
 import re
@@ -6,44 +9,49 @@ import subprocess
 import threading
 import time
 
-
 def hint(appname, usage):
-    # ' remove
-    appname = appname.replace("'", "")
+    '''
+    uyarı mesajı 
+    '''
+    appname = appname.replace("'", "") # ' remove
     try:
         appname = appname.split("/")
         appname = appname[-1]
-    except:
+    except ValueError:
         appname = appname.strip()
     uyari = "\"UYARI !\""
-    mesaj = "\"Yüksek CPU kullanımı tespit edildi \n{} %{} \"".format(appname,usage)
+    mesaj = "\"Yüksek CPU kullanımı tespit edildi \n{} %{} \"".format(appname, usage)
     komut = "notify-send "+uyari+" "+mesaj+" -t 6000 -i hint"
     if not appname in EXCEPTS:
         os.system(komut)
 
 def check_prc():
+    '''
+    çalışan süreçler
+    '''
     dizi = []
-    bash_out = subprocess.check_output("ps aux",stderr=subprocess.STDOUT,shell=True)
+    bash_out = subprocess.check_output("ps aux", stderr=subprocess.STDOUT, shell=True)
     satirlar = bash_out.splitlines()
     for i in satirlar:
-        i = re.sub('([\s]+)', ' ', str(i))
-        dizi.append(i)
+        dizi.append(re.sub(r'([\s]+)', ' ', str(i)))
     dizi.pop(0) # ilk satırı sil
     for i in dizi:
-        i = i.split(" ")
-        cpu_usage,name = float(i[2]),str(i[10])
+        sutunlar = i.split(" ")
+        cpu_usage, name = float(sutunlar[2]), str(sutunlar[10])
         if cpu_usage > HIGH:
-            findapp = threading.Thread(target=hint, args=(name,cpu_usage))
+            findapp = threading.Thread(target=hint, args=(name, cpu_usage))
             findapp.start()
 
-
 def main():
+    '''
+    main loop
+    '''
     while 1:
         check_prc()
-        time.sleep(5)
+        time.sleep(3)
 
 HIGH = 90
-EXCEPTS = ["firefox","chrome","chromium","java","inox","QtWebEngineProcess"]
+EXCEPTS = ["firefox", "chrome", "chromium", "java", "inox", "QtWebEngineProcess"]
 
 if __name__ == '__main__':
     main()
