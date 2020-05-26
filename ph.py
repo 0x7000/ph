ec264016db953c192b468a7bf8ee78070ee42583
command <rekreb81@gmail.com>
2020-05-26 22:30:56 +0300

    zart zurt

--- ph.py
+++ ph.py
@@ -1,40 +1,35 @@
 #!/usr/bin/env python3
-'''
-GPLv3 lisansıyle dağıtılır
-'''
-
 import os
 import re
 import subprocess
 import threading
 import time
 
+
 def hint(appname, usage):
-    '''
-    uyarı mesajı 
-    '''
-    appname = appname.replace("'", "") # ' remove
+    appname = appname.replace("'", "")
+    '''  ' remove '''
     try:
-        appname = appname.split("/")
-        appname = appname[-1]
+        uygulama = appname.split("/")[-1]
+        # print(uygulama)
     except ValueError:
-        appname = appname.strip()
+        uygulama = appname.strip()
     uyari = "\"UYARI !\""
-    mesaj = "\"Yüksek CPU kullanımı tespit edildi \n{} %{} \"".format(appname, usage)
+    mesaj = "\"Yüksek CPU kullanımı tespit edildi \n{} %{} \"".format(uygulama, usage)
     komut = "notify-send "+uyari+" "+mesaj+" -t 6000 -i hint"
-    if not appname in EXCEPTS:
+    if uygulama not in EXCEPTS:
         os.system(komut)
 
+
 def check_prc():
-    '''
-    çalışan süreçler
-    '''
+    # çalışan süreçler
     dizi = []
     bash_out = subprocess.check_output("ps aux", stderr=subprocess.STDOUT, shell=True)
     satirlar = bash_out.splitlines()
     for i in satirlar:
         dizi.append(re.sub(r'([\s]+)', ' ', str(i)))
-    dizi.pop(0) # ilk satırı sil
+    dizi.pop(0)
+    # ilk satırı sil
     for i in dizi:
         sutunlar = i.split(" ")
         cpu_usage, name = float(sutunlar[2]), str(sutunlar[10])
@@ -42,13 +37,12 @@ def check_prc():
             findapp = threading.Thread(target=hint, args=(name, cpu_usage))
             findapp.start()
 
+
 def main():
-    '''
-    main loop
-    '''
     while 1:
         check_prc()
         time.sleep(3)
+
 
 HIGH = 90
 EXCEPTS = ["firefox", "chrome", "chromium", "java", "inox", "QtWebEngineProcess"]
